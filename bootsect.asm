45506384966bff732666e1b699d27395d08b75bf
Cameron Ryan <cjryanwashere@gmail.com>
2024-05-30 15:46:00 -0400

    experiment with memory offsets

--- bootsect.asm
+++ bootsect.asm
@@ -1,25 +1,32 @@
 
 
 
-mov ah, 0x0e
+mov ah, 0x0e		; int 10/ah = 0eh -> scrolling teletype BIOS routine
 
-mov al, 'H'
+mov al, the_secret
 int 0x10
-mov al, 'e'
+
+mov al, [the_secret]
 int 0x10
-mov al, 'l'
-int 0x10 
-mov al, 'l' 
-int 0x10 
-mov al, 'o' 
+
+
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
 int 0x10
+
+mov al, [0x7c1e]
+int 0x10
+
+
+
 
 jmp $			; Jump to the current address
 
-
+the_secret:
+	db "X"
 
 times 510-($-$$) db 0	; Pad the boot sector with zeros
-
 dw 0xaa55		; last two bytes form the magic number, so BIOS knows that it is a boot sector
 
 
