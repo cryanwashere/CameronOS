a1800452cddb8071d7ea7a324ecfdd3eb7bb08ca
Cameron Ryan <cjryanwashere@gmail.com>
2024-06-02 21:52:16 +0000

    untested kernel

--- kernel.c
+++ kernel.c
@@ -33,7 +33,7 @@ enum vga_color {
 	VGA_COLOR_LIGHT_MAGENTA	= 13, 
 	VGA_COLOR_LIGHT_BROWN	= 14, 
 	VGA_COLOR_WHITE		= 15, 
-}
+};
 
 
 /* define the foreground and background of a character with an 8 bit integer. moving right to left, the first 4 bits define the foreground, and the second 4 bits define the background*/
@@ -47,6 +47,88 @@ static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
 	return (uint16_t) uc | (uint16_t) color << 8;
 }
+
+/* Get the length of a string */
+size_t strlen(const char* str)
+{
+	size_t len = 0;
+	while (str[len])
+		len++;
+	return len;
+}
+
+
+/* define constants regarding the dimensions of the terminal */
+static const size_t VGA_WIDTH;
+static const size_t VGA_HEIGHT;
+
+
+
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color; 
+uint16_t* terminal_buffer;
+
+
+void terminal_initialize(void) 
+{
+	terminal_row = 0; 
+	terminal_column = 0; 
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0xB8000;
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = vga_entry(' ', terminal_color);
+		}
+	}
+}
+
+void terminal_setcolor(uint8_t color)
+{
+	terminal_color = color; 
+}
+
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = vga_entry(c, color);
+}
+
+void terminal_putchar(char c)
+{
+	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	if (++terminal_column == VGA_WIDTH){
+		terminal_column = 0;
+		if (++ terminal_row == VGA_HEIGHT) {
+			terminal_row = 0;
+		}
+	}
+}
+
+void terminal_write(const char* data, size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+		terminal_putchar(data[i]);
+}
+
+void terminal_writestring(const char* data)
+{
+	terminal_write(data, strlen(data));
+}
+
+void kernel_main(void)
+{
+	/* Initialize the terminal interface */
+	terminal_initialize();
+
+	/* Write a message to the terminal */
+	terminal_writestring("Hello, World!");
+}
+
+
+
+
+
 
 
 
